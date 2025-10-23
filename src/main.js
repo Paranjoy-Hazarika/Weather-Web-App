39c8c6dbfbebdbbee2e031795212ac6bc739a617
Paranjoy <paranjoyhazarika@gmail.com>
2025-10-23 22:50:58 +0530

    Added pico-css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import '@picocss/pico/css/pico.min.css'
 
 document.querySelector('#app').innerHTML = `
   <div>

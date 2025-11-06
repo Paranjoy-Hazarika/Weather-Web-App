50b29e8df3a2decb9444aac696c06eaa049bcd6f
Paranjoy <paranjoyhazarika@gmail.com>
2025-11-06 21:37:51 +0530

    Added a sample weather data json file

--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
-import './styles/style.css'
+fetch('/src/sample_data/sample_data.json')
+    .then(response => {
+    
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+    
+        return response.json()
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error('Error occurred: ', error))
 
+
+document.querySelector(".search-btn").addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector(".search-container").classList.toggle("active");
+    document.querySelector(".search-bar").classList.toggle("active")
+})

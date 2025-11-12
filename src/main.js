371e7ec429f87749495b6673b357604aae033758
Paranjoy <paranjoyhazarika@gmail.com>
2025-11-12 21:42:07 +0530

    Added the weather update logic

--- src/main.js
+++ src/main.js
@@ -2,8 +2,32 @@ import sample_data from './sample_data/sample_data.json';
 
 import "/node_modules/bootstrap-icons/font/bootstrap-icons.min.css";
 
-import { apiHandling } from './scripts/api.js'
+// Buttons & Containers
 
+const searchBtn = document.querySelector(".search-btn")
+const searchIcon = document.querySelector("#search-icon");
+const searchContainer = document.querySelector(".search-container");
+const searchInput = document.getElementById("search-input")
+const searchBar = document.querySelector(".search-bar")
+const blurContainer = document.querySelector(".blur-container");
+const calendarBtn = document.querySelector(".calendar-btn");
+const nextDaysSection = document.querySelector(".days-section")
+const backToMain = document.querySelector(".back-btn");
+
+const cityBox = document.getElementById("city");
+const dateBox = document.getElementById("date");
+const tempBox = document.getElementById("current-temp");
+const weatherBox = document.getElementById("current-conds");
+const rain = document.getElementById("rain-data")
+const humidity = document.getElementById("humidity-data")
+const wind = document.getElementById("wind-data")
+
+const API_KEY = '75b1486f1ab3fddbcfce6813e460be82'
+
+const date = new Date()
+let cityInput;
+
+const currentDate = date.toLocaleString('en-US', { weekday: 'short', month: 'short', day: '2-digit' });
 
 // API CALLS
 
@@ -15,27 +39,9 @@ import { apiHandling } from './scripts/api.js'
 
 //
 
+dateBox.innerText = `${currentDate}`
 
-console.log(sample_data) // only for a fixed data (use fetch when using an actual api key)
-
-
-// fetch(sample_data)
-//     .then(response => {
-    
-//         if (!response.ok) {
-//             throw new Error(`HTTP error: ${response.status}`);
-//         }
-    
-//         return response.json()
-//     })
-//     .then(data => console.log(data))
-//     .catch(error => console.error('Error occurred: ', error))
-
-
-const searchBtn = document.querySelector(".search-btn")
-const searchIcon = document.querySelector("#search-icon");
-const searchContainer = document.querySelector(".search-container");
-const blurContainer = document.querySelector(".blur-container");
+// Icon and Hidden Section Toggle
 
 const toggleSection = () => {
     searchContainer.classList.toggle("hidden");
@@ -52,6 +58,8 @@ const toggleIcons = () => {
     }
 }
 
+// Event Listener
+
 searchBtn.addEventListener('click', (e) => {
     e.preventDefault();
     
@@ -59,44 +67,99 @@ searchBtn.addEventListener('click', (e) => {
     toggleIcons();
 })
 
-const searchInput = document.getElementById("search-input")
-const searchBar = document.querySelector(".search-bar")
-let value;
-
-const city = document.getElementById("city");
-const date = document.getElementById("date")
-
 searchBar.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    value = searchInput.value;
-    console.log(`Submitted: ${value}`);
+    cityInput = searchInput.value;
+    console.log(`Submitted: ${cityInput}`);
     
-    loadWeather(value);
+    loadWeather(cityInput);
     toggleSection();
     toggleIcons();
 
-    // apiHandling(value, city);
+    apiHandling(cityInput);
     
     searchInput.value = "";
 })
 
-const calendarBtn = document.querySelector(".calendar-btn");
-const nextDaysSection = document.querySelector(".days-section")
 
 calendarBtn.addEventListener("click", (e) => {
     nextDaysSection.classList.toggle("hidden");
     nextDaysSection.style.left = "0%";
 })
 
-const backToMain = document.querySelector(".back-btn");
 
 backToMain.addEventListener("click", () => {
     nextDaysSection.classList.toggle("hidden");
     nextDaysSection.style.left = "100%";
 })
 
-
 const loadWeather = (cityName) => {
-    city.innerText = `${cityName}`;
+    cityBox.innerText = `${cityName}`;
 }
+
+
+// Handling API
+
+const apiHandling = (cityName) => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTPS error: ${response.status}`)
+            }
+
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            let lat = data[0].lat;
+            let lon = data[0].lon;
+
+            weatherHandling(lat, lon)
+            
+        })
+        .catch(error => console.error(`Error occurred: ${error}`))
+}
+
+const weatherHandling = (lat, lon) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+        .then(response => {
+            
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`)
+            }
+
+            return response.json();
+        })
+        .then(data => {
+        
+            console.log(data)
+            
+            const stateName = data.name;
+            const countryCode = data.sys.country;
+            const countryName = new Intl.DisplayNames(["en"], { type: "region" }).of(countryCode);
+            const currentTemp = data.main.temp;
+            const weatherConds = data.weather[0].main;
+            const rainMeasure = data.rain ? data.rain["1h"] || data.rain["3h"] || 0 : 0;
+            const windMeasure = data.wind.speed;
+            const humidityMeasure = data.main.humidity;
+
+            uiUpdate(stateName, countryName, currentTemp, weatherConds, rainMeasure, windMeasure, humidityMeasure);
+            
+        })
+        .catch(error => console.error(`Error occurred: ${error}`))
+    }
+    
+const uiUpdate = (stateName, countryName, current_temp, weather_cond, rain_measure, wind_measure, humidity_measure) => {
+    cityBox.innerHTML = `${stateName}, <br> ${countryName}`;    
+    
+    tempBox.innerText = `${Math.round(current_temp)}`
+    weatherBox.innerText = `${weather_cond}`
+    
+    rain.innerText = `${rain_measure} mm`
+    wind.innerText = `${wind_measure.toFixed(1)} m/s`;
+    humidity.innerText = `${humidity_measure} %`;
+
+}
+
+// Dummy Practice

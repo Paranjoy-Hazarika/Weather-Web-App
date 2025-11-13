e87b5e9a123abf9d8799d6f352871acdc501d4d6
Paranjoy <paranjoyhazarika@gmail.com>
2025-11-13 20:26:12 +0530

    API FIX

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ const humidity = document.getElementById("humidity-data")
 const wind = document.getElementById("wind-data")
 const imgContainer = document.querySelector(".weather-icon");
 
-// const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 // const locaitonURL = ``
 
 const date = new Date()
